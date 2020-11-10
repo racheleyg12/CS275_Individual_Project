6234499e739f48bda561da49f8fa200a02101abf
racheleyg12 <rgoldma1@uvm.edu>
2020-11-10 00:04:25 -0500

    working up to pg201 in Ch11

--- Pokedex/Pokedex/ItemsViewController.swift
+++ Pokedex/Pokedex/ItemsViewController.swift
@@ -44,7 +44,36 @@ class Item: NSObject {
 }
 
 class ItemsViewController: UITableViewController {
+    
     var itemStore: ItemStore!
+    
+    @IBAction func addNewItem(_ sender: UIButton) {
+        // Create a new item and add it to the store
+        let newItem = itemStore.createItem()
+        
+        // Figure out where that item is in the array
+        if let index = itemStore.allItems.firstIndex(of: newItem) {
+            let indexPath = IndexPath(row: index, section: 0)
+            // Insert this new row into the table
+            tableView.insertRows(at: [indexPath], with: .automatic) }
+    
+    }
+    @IBAction func toggleEditingMode(_ sender: UIButton) {
+        // If you are currently in editing mode...
+        if isEditing {
+            // Change text of button to inform user of state
+            sender.setTitle("Edit", for: .normal)
+            // Turn off editing mode
+            setEditing(false, animated: true)
+        
+        } else {
+            // Change text of button to inform user of state
+            sender.setTitle("Done", for: .normal)
+            // Enter editing mode
+            setEditing(true, animated: true)
+        }
+    }
+
 
 //    func numberOfSectionsInTableView(_ tableView: UITableView) -> Int {
 //        return 2
@@ -61,8 +90,6 @@ class ItemsViewController: UITableViewController {
         //return 2
         
     }
-    
-    
     
     //Specifying a Row for the UITableView aka UITableViewCell
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
