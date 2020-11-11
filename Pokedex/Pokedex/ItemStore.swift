73cd7356c3a83b5bf9b3361ad780fb27ff4dd8fd
racheleyg12 <rgoldma1@uvm.edu>
2020-11-11 14:12:18 -0500

    working at Ch11 pg204

--- Pokedex/Pokedex/ItemStore.swift
+++ Pokedex/Pokedex/ItemStore.swift
@@ -9,11 +9,22 @@ import UIKit
 class ItemStore {
     var allItems = [Item]()
     
-    //Initializer to add five random items.
-    init() {
-    for _ in 0..<5 {
-            createItem()
+    func removeItem(_ item: Item) {
+        if let index = allItems.firstIndex(of: item) {
+            allItems.remove(at: index)
         }
+    }
+    
+    func moveItem(from fromIndex: Int, to toIndex: Int) {
+        if fromIndex == toIndex {
+            return
+        }
+        // Get reference to object being moved so you can reinsert it
+        let movedItem = allItems[fromIndex]
+        // Remove item from array
+        allItems.remove(at: fromIndex)
+        // Insert item in array at new location
+        allItems.insert(movedItem, at: toIndex)
     }
     
     func filterItemsBy(_ price: Int = 50) -> [[Item]] {
